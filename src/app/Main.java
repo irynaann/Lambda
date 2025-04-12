f3e2efeedf3b7203f981dc0a600fd3c5c28dcfc7
Iryna Andriichenko <iryna.andriichenko@gmail.com>
2025-04-13 01:04:12 +0200

    Call all implemented logic in main method and print results

--- src/app/Main.java
+++ src/app/Main.java
@@ -6,19 +6,27 @@ import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
+
         MathOperation mathOperation = new MathOperation() {
+            @Override
             public int operate(int a, int b) {
                 return a + b;
-
             }
         };
 
+        int sum = mathOperation.operate(1, 3);
+        System.out.println("1) " + sum);
+
         StringManipulator stringManipulator = str -> str.toUpperCase();
         String result = stringManipulator.convertToUpperCase("cat");
-        System.out.println(result);
+        System.out.println("2) " + result);
 
         Function<String, Integer> result2 = StringListProcessor::countUppercase;
-        Supplier<Integer> randomNum = () -> ThreadLocalRandom.current().nextInt(1,100);
+        int upperCaseSum = result2.apply("BaBy");
+        System.out.println("3) " + upperCaseSum);
 
+        Supplier<Integer> randomNum = () -> ThreadLocalRandom.current().nextInt(1, 100);
+        int randomNumber = randomNum.get();
+        System.out.println("4) " + randomNumber);
     }
 }
